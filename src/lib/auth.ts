1fad60fa9c295266ca87807f4f6ff46c84d81df3
mazen <mazenraafat568@gmail.com>
2026-07-08 17:26:13 +0300

    added protected routing to the account page using Next middleware

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
@@ -7,7 +8,12 @@ export const authConfig :NextAuthConfig = {
             clientId: process.env.AUTH_GOOGLE_ID!,
             clientSecret: process.env.AUTH_GOOGLE_SECRET!
         })
-    ]
+    ],
+    callbacks:{
+        authorized({auth,request}){
+            return !!auth;
+        }
+    }
 }
 
 export const { auth, handlers: {
