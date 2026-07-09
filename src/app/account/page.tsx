75d132dc7621eed5bfaa3d350f99eca893418d41
mazen <mazenraafat568@gmail.com>
2026-07-09 12:48:14 +0300

    implemented a custom signIn page and a custom signIn button to login instead of the provided signIn page provided by NextJs

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,14 +1,20 @@
 import { Metadata } from "next"
+import {auth} from "@/lib/auth"
 
 export const metadata:Metadata={
   title:"Account"
 }
-export default function page() {
+export default async function page() {
+  const session = await auth();
+  const firstName = session?.user?.name?.split(" ")[0];
+  // console.log(firstName);
+  
+
   return (
     <div>
 
         <h1>
-        account
+        Welcome {firstName}
         </h1>
     </div>
   )
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,20 +2,27 @@
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
-export const authConfig :NextAuthConfig = {
+export const authConfig: NextAuthConfig = {
     providers: [
         Google({
             clientId: process.env.AUTH_GOOGLE_ID!,
             clientSecret: process.env.AUTH_GOOGLE_SECRET!
         })
     ],
-    callbacks:{
-        authorized({auth,request}){
+    callbacks: {
+        authorized({ auth, request }) {
             return !!auth;
         }
+    },
+    pages: {
+        signIn: "/login"
     }
 }
 
-export const { auth, handlers: {
+export const {
+     auth, 
+     signIn, 
+     signOut, 
+     handlers: {
     GET, POST
 } } = NextAuth(authConfig);
